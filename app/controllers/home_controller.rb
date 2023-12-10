4f4cbad0b12169863e4823e278674e7e123291de
Lucas Hlongwane <LucasHlongwane6@gmail.com>
2023-12-10 23:23:51 +0200

    bug fix, remove instance variables since they don't persist between different actions and pass as URL parameters

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,7 @@
 class HomeController < ApplicationController
   def index
+    @error_message = params[:error_message]
+    @avatar_url = params[:avatar_url]
   end
 
 
@@ -9,15 +11,16 @@ class HomeController < ApplicationController
     response = openai_service.generate_image(bio)
 
     if response['error']
-      @error_message = "OpenAI API error: #{response['error']['message']}"
+      error_message = "OpenAI API error: #{response['error']['message']}"
+      redirect_to root_path(error_message: error_message)
     else
       if response['data'].present? && response['data'].first['url'].present?
-        @avatar_url = response['data'].first['url']
+        avatar_url = response['data'].first['url']
+        redirect_to root_path(avatar_url: avatar_url)
       else
-        @error_message = 'Failed to retrieve image URL'
+        error_message = 'Failed to retrieve image URL'
+        redirect_to root_path(error_message: error_message)
       end
     end
-
-    redirect_to root_path
   end
 end
